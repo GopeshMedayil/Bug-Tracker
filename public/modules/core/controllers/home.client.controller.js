fc4a02c1e9fc8f1f6f7c4a4eae1b96f8704a5572
GopeshMedayil <medayilgopesh@gmail.com>
2014-09-06 23:29:16 +0530

    Added logic to user login redirection

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,15 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication','$location',
+	function($scope, Authentication,$location) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		console.log("In home controller");
+		console.log($scope.authentication);
+		//Redirect user to login page if not logged in
+		if($scope.authentication.user==''){
+			$location.path('/signin')
+		}
 	}
 ]);
